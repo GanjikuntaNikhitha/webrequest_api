fb8c9242d982f671dbf263133f639d9ecc7cdaef
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-16 15:34:51 +0530

    getting values from parent as user

--- random-card/src/app/card/card.component.ts
+++ random-card/src/app/card/card.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input  } from '@angular/core';
+import {
+  faEnvelope,
+  faMapMarkedAlt,
+  faPhone,
+  faDatabase,
+} from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  @Input()
+  user;
+  // making the variables
+  faEnvelope = faEnvelope;
+  faMapMarkedAlt = faMapMarkedAlt;
+  faPhone = faPhone;
+  faDatabase = faDatabase;
   constructor() { }
 
   ngOnInit(): void {
