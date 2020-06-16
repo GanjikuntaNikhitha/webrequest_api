8e32cb3e73888a7d3790c18209d94eb1edb7be6e
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-16 14:46:42 +0530

    adding httpCliendmodule to app

--- random-card/src/app/app.module.ts
+++ random-card/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// including HTTPCLIENTMODULE in app
+import { HttpClientModule } from '@angular/common/http'; 
+// toster module
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ToastrModule } from "ngx-toastr";
+//font Awesome
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -12,7 +18,12 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // import the module
+    HttpClientModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
